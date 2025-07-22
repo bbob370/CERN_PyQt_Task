47754d3a2f5a43103e878f8d2ef2326ae2b9db41
Max Shen <maxshen2503@gmail.com>
2025-07-22 12:56:59 +0100

    added country data from api
    Implemented the country data from an api in a rough format

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
+import PyQt5.QtNetwork as qtn
+from PyQt5.QtCore import QUrl
+import json
+
 
 class countrySelectWindow(qtw.QWidget):
     def __init__(self):
@@ -15,27 +19,61 @@ class countrySelectWindow(qtw.QWidget):
         #create widgets
         self.country_label = qtw.QLabel('Hello World!')
         self.country_combobox = qtw.QComboBox(self)
-        
-        #add items to combobox
-        country_list = ['China', 'France', 'Switzerland', 'US', 'UK']
-        self.country_combobox.addItems(country_list)
 
         #add widgets
         self.layout().addWidget(self.country_label)
         self.layout().addWidget(self.country_combobox)
 
+        self.network_manager = qtn.QNetworkAccessManager()
+        #network stuff
+        self.network_manager.finished.connect(self.handle_response)
+
         #set connections
-        self.country_combobox.currentTextChanged.connect(self.update_label)
+        #self.country_combobox.currentTextChanged.connect(self.update_label)
     
         #show app
         self.show()
 
-        #network stuff
+        self.access_data()
 
-
+    def access_data(self):
+        print('fetching data')
+        url = QUrl("https://www.apicountries.com/countries")
+        request = qtn.QNetworkRequest(url)
+        self.network_manager.get(request)
+        
+        print('data obtained')
+    
     def update_label(self, text):
         self.country_label.setText(f'Selected: {text}')
 
+    def handle_response(self, reply):
+        print('handling response')
+
+        if reply.error():
+            print("Failed to fetch data.")
+            return
+
+        data = reply.readAll().data()
+        
+        try:
+            json_data = json.loads(data)
+        except Exception as e:
+            print(f"Error parsing data: {e}")
+        
+
+        #get list of countries
+        country_list = []
+
+        for country_dict in json_data:
+            country_list.append(country_dict["name"])
+        
+        #sort list:
+        country_list.sort()
+
+        self.country_combobox.addItems(country_list)
+
+        self.country_combobox.currentTextChanged.connect(self.update_label)
 
 
 app = qtw.QApplication([])
